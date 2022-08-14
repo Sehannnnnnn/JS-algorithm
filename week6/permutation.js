35d0c7af174bd94ab7ebd5b47b21e99937f91690
sehannnnnnn <stanley710@naver.com>
2022-08-14 16:46:02 +0900

    1189 unsolved

--- week6/permutation.js
+++ week6/permutation.js
@@ -1,15 +1,15 @@
 const numbers = ['a','a','b','c'];
 
 const output = [];
-const permutation = (permu, rests) => {
+const permutation = (permu, rests,output) => {
     if (permu.length == numbers.length) {
         return output.push(permu);
     }
     rests.forEach((v,idx) => {
         const rest = [...rests.slice(0,idx), ...rests.slice(idx+1)]
-        permutation([...permu,v],rest);
+        permutation([...permu,v],rest,output);
     });
 }
 
-permutation([], numbers);
+permutation([], numbers,output);
 console.log(output);
---
+++ week8-programmers/1189.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync('/Users/gimsehan/Develop/JS-algorithm/week8-programmers/input.txt').toString().trim().split('\n');
+
+const [R,C,K] = input.shift().split(' ').map(e => +e);
+const map = input.map(line => line.split(''))
+
+const B = K-(R+C-1);
+
+console.log(B);
+const output = [];
+
+function createMoves(R,C,B) {
+    const moves = [];
+    ㄹ
+    return
+}
+function movesPermutation (R,C,B) {
+
+}
